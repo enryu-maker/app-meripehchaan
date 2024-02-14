38734364018ca97e17553ca297e434ff999deeb6
enryu-maker <akifkhan60067@gmail.com>
2024-02-14 21:43:28 +0530

    cc

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import { store } from './Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/'>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
